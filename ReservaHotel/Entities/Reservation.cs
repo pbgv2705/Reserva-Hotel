49fe861e38550fae57c932dd87a3f301175cc273
Paula Gimenez <paulaherbal@gmail>
2022-05-01 20:23:49 -0300

    Projeto Reserva Hotel

---
+++ ReservaHotel/Entities/Reservation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using ReservaHotel.Entities.Exceptions;
+
+namespace ReservaHotel.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+        public int Duration()
+        {
+            // calcula diferença em dias entre duas datas
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException("Reservations dates for update must be future dates");
+
+            }
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+        public override string ToString()
+        {
+            return "Room: "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn
+                + ", check-out: "
+                + CheckOut
+                + ", "
+                + Duration()
+                + " nights";
+        }
+
+    }
+}
